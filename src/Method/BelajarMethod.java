0f205a9b1c8902f4527a1abd71ba0d3ab9dc2782
bayu <bayuteguhpamuji@gmail.com>
2018-03-05 09:37:40 +0700

    modify file BelajarMethod

--- src/Method/BelajarMethod.java
+++ src/Method/BelajarMethod.java
@@ -1,4 +1,38 @@
 package Method;
 
 public class BelajarMethod {
+    public static void main(String[]args){
+        /*pengenalan method
+        * method ini berguna untuk mengelompokkan kumpulan kode yang dapat digunakan berulang2 (regroup and reuse it)
+        * struktur penulisan method adl sbb :
+        * [access modifier] [return type] [nama method]
+        * contoh public void main
+        * public : access modifier
+        * void : tipe data
+        * main : nama method
+        * # calling method
+        * namaMethod() atau bisa dengan object
+        * contoh*/
+        //bisa pake ini jika method nya static krn method dalam main static
+        //playMusic();
+        //kalau method tidak static calling methodnya bisa pake object seperti ini
+        BelajarMethod iniObject = new BelajarMethod();
+        iniObject.playMusic();
+    }
+   /* static boolean btnPlay = true;
+    private static void playMusic(){
+        if (btnPlay){
+            System.out.println("Music is playing");
+        }else{
+            System.out.println("Music is pause");
+        }
+    }*/
+    boolean btnPlay = true;
+    private void playMusic(){
+        if (btnPlay){
+            System.out.println("Music is playing");
+        }else{
+            System.out.println("Music is pause");
+        }
+    }
 }
